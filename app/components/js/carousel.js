5fcf20adb60dc70ad211c061324b72eaeabf8af1
JS-LIU <984003917@qq.com>
2015-08-28 19:32:57 +0800

    1.使用了service模块 2.directive的依赖注入service 3.添加了超级牛逼的triggerOnce方法 4.回家前提交一下

--- app/components/js/carousel.js
+++ app/components/js/carousel.js
@@ -2,44 +2,28 @@
  * Created by 殿麒 on 2015/8/25.
  */
 
+var obj = new Object();
 
-main.directive('carousel',['$swipe',function($swipe){
-    return{
-        restrict:'E',
-        template:'<div class="clearfix carousel-animation" ng-transclude></div>',
-        transclude:true,
-        link:function($scope,ele){
-            var imgLen = $scope.imgs.length,
-                window_width = window.screen.availWidth,
-                bigWidth = imgLen * window_width,
-                moveEle = ele.children();
-            ele.css({'width':window_width + 'px','display':'block'});
-            moveEle.css({height:'395px',width:bigWidth + 'px'});
-
-            $swipe.bind(ele, {
-                'start': function(coords) {
-                    startX = coords.x;
-                    pointX = coords.y;
-                },
-                'move': function(coords) {
-                    var delta = coords.x - pointX;
-                },
-                'end': function(coords) {
-                },
-                'cancel': function(coords) {
-                }
-            });
-
-
-
+obj.triggerOnce = function(fn) { //控制让函数只触发一次
+    return function() {
+        try {
+            fn.apply(this, arguments);
+        }
+        catch (e) {
+            var txt = "There was an error on this page.\n\n";
+            txt += "Error message: " + e.message + "\n\n";
+            txt += "Error name: " + e.name + "\n\n";
+        }
+        finally {
+            fn = null;
         }
     }
-}])
+}
 
-main.controller('myInterval',function($scope,$interval){
-
-    function carouselTrack(){
-        var window_width = window.screen.availWidth;
+main.service('carouselTrack',function(){
+    var self = this;
+    var window_width = window.screen.availWidth;
+    this.slideLeft = function (){
         var $_carouselAnimate =  $('.carousel-animation');
         var $_carouselAnimateWidth = parseFloat($_carouselAnimate.css('width'));
         $_carouselAnimate.animate({
@@ -51,8 +35,64 @@ main.controller('myInterval',function($scope,$interval){
             }
         })
     }
+    this.slideRight = function(){
+        var $_carouselAnimate =  $('.carousel-animation');
+        $_carouselAnimate.animate({
+            marginLeft:'+=' + window_width + 'px'
+        },300,function(){
+            var mL = parseFloat($_carouselAnimate.css('marginLeft'));
+            if( mL - 2 * window_width > 0){
+                $_carouselAnimate.css('marginLeft','0px');
+            }
+        })
+    }
 
-    $interval(carouselTrack,$scope.myInterval);
+});
+
+main.directive('carousel',['$swipe','carouselTrack',function ($swipe,carouselTrack){
+    function link($scope,ele){
+        var imgLen = $scope.imgs.length,
+            window_width = window.screen.availWidth,
+            bigWidth = imgLen * window_width,
+            moveEle = ele.children();
+
+        $scope.w = bigWidth;
+
+        ele.css({'width':window_width + 'px','display':'block'});
+        moveEle.css({height:'395px',width:bigWidth + 'px'});
+        var x,x1;
+
+        $swipe.bind(ele, {
+            'start': function(coords) {
+                x = coords.x;
+            },
+            'move': function(coords) {
+                x = x1 || coords.x;
+                x1 = coords.x;
+                var dif = x - x1;
+                //  swipe-left
+                if(dif > 0){
+                    carouselTrack.slideLeft();
+                }
+            },
+            'end': function() {
+                console.log(1);
+            },
+            'cancel': function() {
+                console.log(2);
+            }
+        });
+    }
+    return{
+        restrict:'E',
+        template:'<div class="clearfix carousel-animation" ng-transclude></div>',
+        transclude:true,
+        link:link
+    }
+}])
+
+main.controller('myInterval',function($scope,$interval,carouselTrack){
+    //$interval(carouselTrack.slideLeft,$scope.myInterval);
 })
 
 
