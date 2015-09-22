900ee987be11f7d2a30037f93d9a01129b88e690
JS-LIU <984003917@qq.com>
2015-09-22 20:54:03 +0800

    先提交下回家再说

--- app/components/js/showWay.js
+++ app/components/js/showWay.js
@@ -44,39 +44,113 @@ purchase.controller('showWay',['$rootScope','$scope',function($rootScope,$scope)
     }
 }]);
 
+
+purchase.controller('goodsCart',['$rootScope','$scope',function($rootScope,$scope){
+
+}]);
+
+purchase.controller('goodsItem',['$rootScope','$scope',function($rootScope,$scope){
+    var goodscartListModel = $rootScope.GOODS_CARTLIST;
+    $rootScope.DIALOG_SHOW = false;
+    var i = $scope.$index;
+    var goodsPrice = goodscartListModel[i].price;
+    $scope.isChecked = true;
+    $scope.checkGoods = function(){
+        $scope.isChecked = !$scope.isChecked;
+        var goodsMoney = goodsPrice  * goodscartListModel[i].num;
+        if($scope.isChecked == false){
+            $rootScope.TOTLE_MONEY -= goodsMoney;
+            $rootScope.ALLCHECKED = false;
+        }else{
+            $rootScope.TOTLE_MONEY += goodsMoney;
+            // 判断是否全选
+            if($('.goodsCheck').hasClass('checked')){
+                $rootScope.ALLCHECKED = true;
+            }
+
+        }
+    }
+
+    //  购物车[-]
+    $scope.subtract = function($index){
+        var $_self = $('.J_goodsItem').eq($index);
+
+        if(goodscartListModel[i].num == 1){
+            //  弹出对话框
+            $rootScope.DIALOG_SHOW = true;
+            //  点击取消隐藏对话框
+            $rootScope.NOTREMOVE_GOODS = function(){
+                $rootScope.DIALOG_SHOW = false;
+            }
+            //  点击确认删除当前商品
+            $rootScope.REMOVE_GOODS = function(){
+                $rootScope.DIALOG_SHOW = false;
+                $_self.remove();
+            }
+        }else{
+            goodscartListModel[i].num --;
+        }
+        if($scope.isChecked == true){
+            $rootScope.TOTLE_MONEY -= goodsPrice;
+        }
+    }
+    //  购物车[+]
+    $scope.addGoods = function(){
+        goodscartListModel[i].num ++;
+        if($scope.isChecked == true){
+            $rootScope.TOTLE_MONEY += goodsPrice;
+        }
+    }
+}]);
+
+
 purchase.controller('goodscartBottom',['$rootScope','$scope',function($rootScope,$scope){
     $scope.showgC = true;
 
     $scope.showgoodsCart = function(){
         $scope.showgC = !$scope.showgC;
     }
+    $rootScope.ALLCHECKED = true;
 
+    //  全选
+    $scope.checkAll = function(){
+        $rootScope.ALLCHECKED = !$rootScope.ALLCHECKED;
+
+        //  非全选状态下
+        if($rootScope.ALLCHECKED == true){
+            var goodscartListModel = $rootScope.GOODS_CARTLIST;
+
+            for(var i = 0,len = goodscartListModel.length; i < len;i++){
+                $rootScope.TOTLE_MONEY += (goodscartListModel[i].num * goodscartListModel[i].price);
+            }
+            //  上面所有的都有对勾
+            $('.goodsCheck').addClass('checked');
+
+        }else{
+            $rootScope.TOTLE_MONEY = 0;
+            $('.goodsCheck').removeClass('checked');
+        }
+    }
 }]);
 
-purchase.controller('goodsCart',['$rootScope','$scope',function($rootScope,$scope){
-    var goodscartListModel = $rootScope.GOODS_CARTLIST;
-    $rootScope.DIALOG_SHOW = false;
+purchase.directive('dialog',function(){
+    function link($scope,ele){
+        var window_width = window.screen.availWidth,
+            dialog_left = (window_width - 235) / 2,
+            window_height = window.screen.availHeight,
+            dialog_header = parseFloat($('.dialog-header').css('height'))||0,
+            dialog_center =  parseFloat($('.dialog-center').css('height')) || 0,
+            dialog_bottom =  parseFloat($('.dialog-bottom').css('height')) || 0,
+            dialog_height = dialog_header + dialog_center + dialog_bottom,
+            dialog_top = (window_height - dialog_height) / 2;
 
-    $scope.subtract = function($index){
-        if(goodscartListModel[$index].num == 1){
-            $rootScope.DIALOG_SHOW = true;
-        }else{
-            goodscartListModel[$index].num --;
-        }
-
-
-    }
-    $scope.addGoods = function($index){
-        goodscartListModel[$index].num ++;
-
+        ele.css({'left':dialog_left + 'px','top':dialog_top + 'px'});
     }
 
-    $rootScope.NOTREMOVE_GOODS = function(){
-
+    return {
+        restrict:'E',
+        template:'<div class="dialog-main" ng-transclude></div>',
+        transclude:true,
+        link:link
     }
-    $rootScope.REMOVE_GOODS = function(){
-
-    }
-
-
-}])
+});
