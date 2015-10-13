4f0ba196cebbe2b22ae363956a71993055b18e37
JS-LIU <984003917@qq.com>
2015-10-13 22:28:56 +0800

    搜索地址==！ 又codereview了把rootscope上面的可以挂到$scope上挂到$scope上啦 部署了服务器

--- app/components/js/addGoodscart/changeLocInfo.js
+++ app/components/js/addGoodscart/changeLocInfo.js
@@ -6,13 +6,13 @@ purchase.factory("get_location",function(){
      *  代码来源：http://lbs.amap.com/api/javascript-api/example/p/1602-2/
      *  作用：得到当前位置
      */
+    var map = new AMap.Map('mapContainer', {
+        resizeEnable: true,
+        zoom:16
+    });
 
     function geocoder(lnglatXY,$scope) {
-        $scope.map = new AMap.Map("mapContainer", {
-            resizeEnable: true,
-            center: lnglatXY,       //地图中心点
-            zoom: 16                //地图显示的缩放级别
-        });
+        map.setCenter(lnglatXY);
         var MGeocoder;
         //加载地理编码插件
         AMap.service(["AMap.Geocoder"], function() {
@@ -21,19 +21,20 @@ purchase.factory("get_location",function(){
                 extensions: "all"
             });
             //逆地理编码
-            MGeocoder.getAddress(lnglatXY, function(status, result) {
+            MGeocoder.getAddress(lnglatXY, function(status,result) {
                 if (status === 'complete' && result.info === 'OK') {
-                    geocoder_CallBack(result,lnglatXY,$scope);
+                    geocoder_CallBack(result,$scope,lnglatXY);
                 }
             });
         });
     }
 
-    function geocoder_CallBack(data,lnglatXY,$scope) {
+    function geocoder_CallBack(data,$scope,lnglatXY) {
         //返回地址描述
         $scope.address = data.regeocode.formattedAddress;
         console.log($scope.address);
-        $scope.LNGLAT = {
+        autoSearch($scope.address,$scope);
+        $scope.lnglat = {
             positionX:lnglatXY[0],
             positionY:lnglatXY[1],
             addressInfo:$scope.address,
@@ -48,14 +49,13 @@ purchase.factory("get_location",function(){
      *   代码来源：http://localhost:63342/WaterSending_angularTest/app/06-main.html#/
      *   作用：获取【当前地图中心经纬度】
      */
-    function changeMyPosition($scope,func){
-        $scope.map.on('dragend',function(){
-            var nowLocation = [$scope.map.getCenter().lng,$scope.map.getCenter().lat];
-            console.log(nowLocation);
+    function changeMyPosition(func){
+        map.on('dragend',function(){
+            var nowLocation = [map.getCenter().lng,map.getCenter().lat];
             func(nowLocation);
 
             setTimeout(function(){
-                var correct = [$scope.map.getCenter().lng,$scope.map.getCenter().lat];
+                var correct = [map.getCenter().lng,map.getCenter().lat];
                 if(correct[0] != nowLocation[0] || correct[1] != nowLocation[1]){
                     func(correct);
                     console.log('矫正惯性误差成功...');
@@ -64,9 +64,50 @@ purchase.factory("get_location",function(){
         })
     }
 
+    //输入提示
+    function autoSearch(keywords,$scope){
+        var auto;
+        //加载输入提示插件
+        AMap.service(["AMap.Autocomplete"], function() {
+            var autoOptions = {
+                city: "" //城市，默认全国
+            };
+            auto = new AMap.Autocomplete(autoOptions);
+            console.log(keywords);
+            //查询成功时返回查询结果
+            if (keywords.length > 0) {
+                auto.search(keywords, function(status, result) {
+                    $scope.location = autocomplete_CallBack(result);
+                    $scope.$apply();
+                });
+            }
+        });
+    }
+
+    //输出输入提示结果的回调函数
+    function autocomplete_CallBack(data) {
+        var location = [];
+        var tipArr = data.tips;
+        if (tipArr && tipArr.length > 0) {
+            for (var i = 0; i < tipArr.length; i++) {
+                var lng = tipArr[i].location.lng;
+                var lat = tipArr[i].location.lat;
+                var lnglat = [lng,lat];
+                var obj = {loc:tipArr[i].name,lnglat:lnglat};
+                location.push(obj);
+            }
+
+            return location;
+        } else {
+            console.log('error');
+        }
+    }
+
+
     return {
         getLocation:geocoder,
-        resetMyPosition:changeMyPosition
+        resetMyPosition:changeMyPosition,
+        search:autoSearch
     }
 });
 
@@ -75,8 +116,15 @@ purchase.controller('changeLocInfo',function($scope,get_location,$cookieStore){
     var d = [lnglat.positionX,lnglat.positionY];
     get_location.getLocation(d,$scope);
 
-    get_location.resetMyPosition($scope,function(location){
-        //console.log(location);
-        //get_location.getLocation(location,$scope);
-    })
+    //  时时获取地理位置
+    get_location.resetMyPosition(function(location){
+        //  周边位置
+        get_location.getLocation(location,$scope);
+    });
+
+    //  搜索位置
+    $scope.getmyloc = function(e){
+        var keywords = $(e.target).val();
+        get_location.search(keywords,$scope);
+    }
 });
