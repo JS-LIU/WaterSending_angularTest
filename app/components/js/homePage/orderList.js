5d3bcfe4502090920178b6eb5fffefdfbacc2d8b
JS-LIU <984003917@qq.com>
2015-10-30 14:14:08 +0800

    fixedBugs

--- app/components/js/homePage/orderList.js
+++ app/components/js/homePage/orderList.js
@@ -11,15 +11,19 @@ main.controller('orderListModel',function($rootScope,$scope,mainPost,getAccessIn
         var data = {
             accessInfo:accessInfo,
             requestPageInfo:requestPageInfo,
-            sign:'sign'
+            sign:'sign',
+            clientOrderState:null
         }
+        $scope.orderDetails = function(i){
+            console.log(i);
+        }
+
         console.log(data);
         var path = 'order/list'
-        mainPost.postData(data,path).success(function(data){
-            console.log(data);
-
-        });
+        //mainPost.postData(data,path).success(function(data){
+        //    console.log(data);
+        //});
     }else{
-        console.log('请登录');
+        window.location.href = "http://114.251.53.22/huipaywater/pages/water/app/07-log.html"
     }
 });
