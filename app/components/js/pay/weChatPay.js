9dd3bc672af6afcbf1bc9f0dda5305ae7e664450
JS-LIU <984003917@qq.com>
2015-11-10 21:56:27 +0800

    fixed bugs ==!

--- app/components/js/pay/weChatPay.js
+++ app/components/js/pay/weChatPay.js
@@ -2,65 +2,68 @@
  * Created by 殿麒 on 2015/10/28.
  */
 var pay = angular.module('WXpay', ['ngRoute','ngCookies']);
-
-pay.controller('orderModel',function($rootScope,$scope,$cookieStore,$location){
+function GetQueryString(name)
+{
+    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if(r!=null)return  unescape(r[2]); return null;
+}
+pay.controller('orderModel',function($rootScope,$scope,$cookieStore,$location,purchasePost,log,getAccessInfo){
     var order = $cookieStore.get('orderId');
     $scope.orderId = order["orderId"];
     $scope.payMoney = order["final_fee"];
-
-    if($location.url() == ''){
-        window.location.href = "weixin://open.weixin.qq.com/conn" +
-            "ect/oauth2/authorize?appid=wxdab7bbbbcba36617&redi" +
-            "rect_uri=http:http://www.huipay.com/huipaywater/app" +
-            "/09-payPage.html&response_type=code&scope=snsapi_b" +
-            "ase&state=1#wechat_redirect";
+    var code = GetQueryString('code');
+    if(!code){
+        window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxdab7bbbbcba36617&redirect_uri=http://www.huipay.com/huipaywater/app/09-payPage.html&response_type=code&scope=snsapi_base&state=1#wechat_redirect";
     }else{
-        $rootScope.CODE = $location.url();
-    }
-});
-pay.controller('WXController',function($rootScope,$scope,$cookieStore,purchasePost,log,getAccessInfo){
-
-    $scope.toPay = function(){
-        var order = $cookieStore.get('orderId');
-        var data = {
-            accessInfo:getAccessInfo.loginAccessInfo(),
-            channel:33,
-            orderId:order["orderId"],
-            resubmit:false,
-            sign:'sign',
-            code:$rootScope.CODE
-        }
-        var path = "pay/confirm";
-        purchasePost.postData(data,path).success(function(data){
-            var data = data;
-            function onBridgeReady(){
-                WeixinJSBridge.invoke(
-                    'getBrandWCPayRequest', {
-                        "appId" : data.appId,                                 //公众号名称，由商户传入
-                        "timeStamp":data.timeStamp,                           //时间戳，自1970年以来的秒数
-                        "nonceStr" : data.nonceStr,                           //随机串
-                        "package" : data.package,
-                        "signType" : "MD5",                                   //微信签名方式：
-                        "paySign" : data.paySign                              //微信签名
-                    },
-                    function(res){
-                        if(res.err_msg == "get_brand_wcpay_request：ok" ) {}   // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
-                    }
-                )
+        $scope.code = GetQueryString('code');
+        $scope.toPay = function(){
+            alert($scope.code);
+            var order = $cookieStore.get('orderId');
+            var data = {
+                accessInfo:getAccessInfo.loginAccessInfo(),
+                channel:33,
+                orderId:order["orderId"],
+                resubmit:false,
+                sign:'sign',
+                code:$scope.code
             }
-            if (typeof WeixinJSBridge == "undefined"){
-                if( document.addEventListener ){
-                    document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
-                }else if (document.attachEvent){
-                    document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
-                    document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+            var path = "pay/confirm";
+            purchasePost.postData(data,path).success(function(data){
+                var data = data;
+                function onBridgeReady(){
+                    WeixinJSBridge.invoke(
+                        'getBrandWCPayRequest', {
+                            "appId" : data.appId,                                 //    公众号名称，由商户传入
+                            "timeStamp":data.timeStamp,                           //    时间戳，自1970年以来的秒数
+                            "nonceStr" : data.nonceStr,                           //    随机串
+                            "package" : data.package,
+                            "signType" : "MD5",                                   //    微信签名方式：
+                            "paySign" : data.paySign                              //    微信签名
+                        },
+                        function(res){
+                            if(res.err_msg == "get_brand_wcpay_request：ok" ) {}   //    使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
+                        }
+                    )
                 }
-            }else{
-                onBridgeReady();
-            }
-        });
+                if (typeof WeixinJSBridge == "undefined"){
+                    if( document.addEventListener ){
+                        document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+                    }else if (document.attachEvent){
+                        document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+                        document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+                    }
+                }else{
+                    onBridgeReady();
+                }
+            });
+        }
     }
+
 });
+//pay.controller('WXController',function($rootScope,$scope,$cookieStore,purchasePost,log,getAccessInfo){
+//
+//});
 
 pay.factory('purchasePost',function($http){
     var url = 'http://www.huipay.com/huipaywater/';
